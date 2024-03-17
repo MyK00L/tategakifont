a996fd47fa5e6d2846d49f711aef2fc213edad80
Michael Chelli <michaelchelli00@gmail.com>
2024-03-17 12:53:00 +0100

    better defaults and more options

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def eprint(*args, **kwargs):
 # Meaning of the table values:
 # U: characters which are displayed upright, with the same orientation as they appears in the code charts.
 # R: characters which are displayed sideways, rotated 90 degrees clockwise compared to the code charts.
-# T, Tu, Tr: characters which are not just upright or sideways, but require a different glyph than in the code charts when used in vertical texts. In addition, Tu indicates that as a fallback, the character can be displayed with the code chart glyph upright; similarly, Tr indicates a possible fallback using the code chart glyph rotated 90 degrees clockwise. 
+# T, Tu, Tr: characters which are not just upright or sideways, but require a different glyph than in the code charts when used in tertical texts. In addition, Tu indicates that as a fallback, the character can be displayed with the code chart glyph upright; similarly, Tr indicates a possible fallback using the code chart glyph rotated 90 degrees clockwise.
 # see https://www.unicode.org/reports/tr50/tr50-8.html
 def get_table():
     def line_to_range_type(x):
@@ -33,19 +33,36 @@ def get_table():
 
 # Returns true if the font has a sub for vertical text associated in the font
 def has_vert_sub(g):
-    return any((vs in ' '.join(map(lambda x: x[0], g.getPosSub("*"))) for vs in ["vert", "vrt", "Vert"]) )
+    return any((vs in ' '.join(map(lambda x: x[0], g.getPosSub("*"))) for vs in ["vert", "vrt", "vkna", "Vert"]) )
+
+def table_at(table, code):
+    lb = -1
+    ub = len(table)
+    while lb<ub-1:
+        m = (lb+ub)//2
+        if table[m][0][0] <= code:
+            lb = m
+        else:
+            ub = m
+    if table[lb][0][0] <= code and code <= table[lb][0][1]:
+        return table[lb][1]
+    return "None"
 
 def main():
     # Parse args
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="input font file name")
     parser.add_argument("output", help="output font file name")
-    parser.add_argument("--round", action="store_true", help="round all glyph points in the output font")
+    parser.add_argument("--no-round", action="store_true", help="do not round all glyph points in the output font")
+    parser.add_argument("--only-sub-table", action="store_true", help="apply glyph substitutions only if they are marked as T, Tu or Tr in the vertical orientation property table")
+    parser.add_argument("--rotation-center", choices=["halfemdesc","halfem","bb"],default="halfemdesc",help="middle point for rotation:\nbb = bounding box of all glyphs to be rotated,\nhalfem = (em/2,em/2),\nhalfemdesc = (em/2,em/2-descent)")
     args = parser.parse_args()
     
+    table = get_table()
+
     # Open intput font
     font = fontforge.open(args.input)
-    
+
     # Modify name
     font.fontname+="-Rotated"
     font.fullname+=" Rotated"
@@ -55,6 +72,8 @@ def main():
     for g in list(filter(has_vert_sub, font.glyphs())):
         if g.unicode==-1:
             continue
+        if args.only_sub_table and (table_at(table,g.unicode) not in ["T","Tr","Tu"]):
+            continue;
         c=chr(g.unicode)
         pos_sub = list(filter(lambda s: any(vs in s for vs in ["vert", "vrt", "Vert"]), map(lambda x: x[0], g.getPosSub("*"))) )[0]
         pos_sub = g.getPosSub(pos_sub)[0]
@@ -77,22 +96,34 @@ def main():
             eprint("maybe unhandled: unknown",pos_sub,c,g.glyphname)
     
     # Select the glyphs that need to be rotated
-    table = get_table()
     ranges = map(lambda x: x[0], filter(lambda x: x[1]=="U" or x[1]=="Tu", table))
     for rang in ranges: # select according to the table
         font.selection.select(("ranges","more"),*rang)
     for guni in subbed: # also select all those that have been substituted
         font.selection.select(("more",),guni)
 
-    # Rotate selected glyphs based on the square center
+    # Rotate selected glyphs
     cx = font.em/2
-    cy = font.em/2
+    cy = font.em/2-font.descent
+    if args.rotation_center == "bb":
+        bbb = [font.em*8,font.em*8,-font.em*8,-font.em*8]
+        for g in font.selection.byGlyphs:
+            bb = g.boundingBox()
+            bbb[0] = min(bbb[0],bb[0])
+            bbb[1] = min(bbb[1],bb[1])
+            bbb[2] = max(bbb[2],bb[2])
+            bbb[3] = max(bbb[3],bb[3])
+        cx = (bb[2]+bb[0])/2
+        cy = (bb[3]+bb[1])/2
+    elif args.rotation_center == "halfem":
+        cx = font.em/2
+        cy = font.em/2
     trcen = psMat.translate(-cx,-cy)
     rotcen = psMat.compose(trcen, psMat.compose(psMat.rotate(math.radians(90)), psMat.inverse(trcen)))
     font.transform(rotcen)
     font.selection.none()
 
-    if args.round:
+    if not args.no_round:
         font.selection.all()
         font.round()
         font.selection.none()
